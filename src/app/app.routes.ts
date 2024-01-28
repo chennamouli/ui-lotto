824245f7622887c3af52cdc430036bf7c861b6bb
Chandramouli Gajam <cgajam@ChandraoulisMBP.attlocal.net>
2024-01-28 16:08:08 -0600

    Fix search filter in data table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { Pick4Component } from './pick4/pick4.component';
 import { LottoComponent } from './lotto/lotto.component';
 
 export const routes: Routes = [
-    {path: 'home', component: CashfiveComponent},
+    {path: '', component: CashfiveComponent},
     {path: 'lotto', component: LottoComponent},
     {path: 'pick4', component: Pick4Component},
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    // {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', component: CashfiveComponent}
 ];
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
-import { ColDef, GridReadyEvent, IDateFilterParams } from 'ag-grid-community';
+import { ColDef, GridReadyEvent, IDateFilterParams, ITextFilterParams, GridApi } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule, DatePipe } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-data-table',
@@ -32,6 +33,7 @@ import { CommonModule, DatePipe } from '@angular/common';
     MatDatepickerModule,
     FormsModule,
     CommonModule,
+    FlexLayoutModule,
     ReactiveFormsModule,
   ],
   templateUrl: './data-table.component.html',
@@ -41,7 +43,7 @@ import { CommonModule, DatePipe } from '@angular/common';
 export class DataTableComponent implements OnInit {
   @Input() game: any | undefined;
   @Input() data: any[] | undefined;
-  private gridApi: any;
+  private gridApi: GridApi | undefined;
   public columnDefs: ColDef[] = [];
   public columnDefs2: ColDef[] = [
     { field: 'athlete' },
@@ -79,17 +81,31 @@ export class DataTableComponent implements OnInit {
     this.gridApi = params.api;
   }
 
+  toggleOptionalColumns(){
+    this.gridApi?.getColumnDefs()?.forEach((col: ColDef) => {
+      if(['Num1'].indexOf(col.field as any) >=0)  {
+        col.hide = false;
+      }
+    })
+    this.gridApi?.refreshCells();
+  }
+
   getColDefs() {
     if (!this.data) return;
     if(this.columnDefs) return this.columnDefs;
     const columnDefs: ColDef[] = [];
     Object.keys(this.data[0]).forEach(key => {
       if (key.toLowerCase().indexOf('date') >= 0) {
-        columnDefs.push({ 'field': key, 'filter': 'agDateColumnFilter', 'filterParams': filterParams });
+        columnDefs.push({ 'field': key, pinned: 'left', 'filter': 'agDateColumnFilter', 'filterParams': filterParams });
+      } else if (key.toLowerCase().indexOf('SortedNumberArray'.toLowerCase()) >= 0) {
+        columnDefs.push({ 'field': key, 'filter': 'agTextColumnFilter', 'filterParams': customTextFilterParams });
+      } else if(['Num1','Num2','Num3','Num4','Num5'].indexOf(key) >= 0) {
+        columnDefs.push({ 'field': key, hide: true });
       } else {
         columnDefs.push({ 'field': key });
       }
     });
+    this.columnDefs = columnDefs;
     console.log('Coldefs: ', columnDefs)
     return columnDefs;
   }
@@ -113,5 +129,46 @@ var filterParams: IDateFilterParams = {
     return 0;
   },
 };
+
+var customTextFilterParams: ITextFilterParams = {
+  textMatcher: ({ filterOption, value, filterText }) => {
+      if (filterText == null) {
+          return false;
+      }
+      switch (filterOption) {
+          case 'contains':
+              return containsAllElements(value.split(/[,\s-]+/), filterText.trim().split(/[,\s-]+/)); // value.indexOf(filterText) >= 0;
+          case 'notContains':
+              return doesNotContainAnyElement(value.split(/[,\s-]+/), filterText.trim().split(/[,\s-]+/)); // value.indexOf(filterText) < 0;
+          case 'equals':
+              return value === filterText;
+          case 'notEqual':
+              return value != filterText;
+          case 'startsWith':
+              return value.indexOf(filterText) === 0;
+          case 'endsWith':
+              const index = value.lastIndexOf(filterText);
+              return index >= 0 && index === (value.length - filterText.length);
+          default:
+              // should never happen
+              console.warn('invalid filter type ' + filterOption);
+              return false;
+      }
+  }
+}
+
+function findCommonValues(array1: any[], array2: any[]) {
+  if(!Array.isArray(array1) || !Array.isArray(array2)) return [];
+  return array1.filter(value => array2.includes(value));
+}
+
+function containsAllElements(array1: any[], array2: any[]) {
+  if(!Array.isArray(array1) || !Array.isArray(array2)) false;
+  return array2.every(element => array1.includes(element));
+}
+
+function doesNotContainAnyElement(array1: any[], array2: any[]) {
+  return !array2.some(element => array1.includes(element));
+}
 
 
