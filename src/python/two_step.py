68a7bd2edd28340e60c5969a366c429a150b8755
Chandramouli Gajam <cgajam@ChandraoulisMBP.attlocal.net>
2024-02-22 13:52:24 -0600

    Built again!

--- src/python/two_step.py
+++ src/python/two_step.py
@@ -17,21 +17,40 @@ def check_for_repeated_numbers(data: list, last_n_games: int):
         #     print(' ')
 
 
+def check_for_repeated_ball(data: list, last_n_games: int):
+    for index, item in enumerate(data):
+        current_sorted_array = [item["BALL"]]
+        previous_sorted_balls = [other_item["BALL"] for other_item in data[index+1 : index+1+last_n_games]]
+        print('previous_sorted_balls', previous_sorted_balls)
+        common_values = np.intersect1d(current_sorted_array, previous_sorted_balls)
+        item[f'RepeatedBallsFromLast{last_n_games}Games'] = len(common_values) > 0
+
+
 game = TWO_STEP
 game_url = TWO_STEP_URL
 game_columns = TWO_STEP_COLUMNS
-json_data = retrieve_game_data(game, [LOTTERY_BASE_URL + game_url], game_columns)
-# json_data = retrieve_game_data(game, ['src/assets/'+game.lower()+'.csv'], game_columns)
+# json_data = retrieve_game_data(game, [LOTTERY_BASE_URL + game_url], game_columns)
+json_data = retrieve_game_data(game, ['src/assets/'+game.lower()+'.csv'], game_columns)
 updatePred1(json_data)
 updatePred2(json_data, 35)
-check_for_repeated_numbers(json_data, 1)
-check_for_repeated_numbers(json_data, 2)
-check_for_repeated_numbers(json_data, 3)
-check_for_repeated_numbers(json_data, 4)
-check_for_repeated_numbers(json_data, 5)
-check_for_repeated_numbers(json_data, 6)
-check_for_repeated_numbers(json_data, 7)
-check_for_repeated_numbers(json_data, 15)
+# check_for_repeated_numbers(json_data, 1)
+# check_for_repeated_numbers(json_data, 2)
+# check_for_repeated_numbers(json_data, 3)
+# check_for_repeated_numbers(json_data, 4)
+# check_for_repeated_numbers(json_data, 5)
+# check_for_repeated_numbers(json_data, 6)
+# check_for_repeated_numbers(json_data, 7)
+# check_for_repeated_numbers(json_data, 10)
+# check_for_repeated_numbers(json_data, 15)
+
+check_for_repeated_ball(json_data, 1)
+check_for_repeated_ball(json_data, 3)
+check_for_repeated_ball(json_data, 5)
+check_for_repeated_ball(json_data, 10)
+check_for_repeated_ball(json_data, 15)
+check_for_repeated_ball(json_data, 20)
+check_for_repeated_ball(json_data, 25)
+check_for_repeated_ball(json_data, 35)
 save_json_to_file(json_data, 'src/assets/'+game.lower()+'.json')
 
 print(f"Finished!")
