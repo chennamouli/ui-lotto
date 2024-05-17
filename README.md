85a83ff2cc94b2732a61b485e6bb3dc9385a7afa
Chandramouli Gajam <cgajam@ChandraoulisMBP.attlocal.net>
2024-05-17 15:07:46 -0500

    Built again!

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # UiLotto
 
+To access the app on the browser, navigate to `https://chennamouli.github.io/ui-lotto/`.
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.1.
 
 ## Development server
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -9,7 +9,7 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     importProvidersFrom(DatePipe),
     importProvidersFrom(HttpClientModule),
